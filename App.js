0dcebc2b7716f7c2400f533f40dfa6c4e27a3a80
jmallon <jmallon@ala-jmallon-l1.corp.ad.wrs.com>
2014-02-10 16:17:33 -0800

    Render table with data

--- App.js
+++ App.js
@@ -27,6 +27,9 @@ Ext.define('CustomApp', {
 		projects.add('15106843053', 'Tools');
 		projects.add('12168776634', 'Tools');
 
+		var programs = ['CGCS', 'VxWorks ST', 'IOT', 'Networking',
+		                    'OVP', 'Linux', 'VxWorks 653 3.0', 'VxWorks 7', 'Tools'];
+
 		var today = Rally.util.DateTime.toIsoString(new Date());
 
 		var releaseDateFilter = Ext.create('Rally.data.wsapi.Filter', {
@@ -108,46 +111,47 @@ Ext.define('CustomApp', {
 														myStore.remove(feature);
 													}
 													else {
-														var projectName = projects.get(featureProjectRefID);
-														console.log(projectName);
-														if (! total.get(projectName)) {
-															total.add(projectName, 1);
+														var program = projects.get(featureProjectRefID);
+														console.log(program);
+														if (! total.get(program)) {
+															total.add(program, 1);
 														}
 														else {
-															var totalTmp = total.get(projectName);
+															var totalTmp = total.get(program);
 															totalTmp++;
-															total.replace(projectName, totalTmp);
+															total.replace(program, totalTmp);
 														}
-														console.log(total.get(projectName));
+														console.log(total.get(program));
 														var featureTracking = feature.get('c_PATracking');
 														// console.log(featureTracking);
 														if (featureTracking == "Review") {
-															if (! reviewed.get(projectName)) {
-																reviewed.add(projectName, 1);
+															if (! reviewed.get(program)) {
+																reviewed.add(program, 1);
 															}
 															else {
-																var reviewedTmp = reviewed.get(projectName);
+																var reviewedTmp = reviewed.get(program);
 																reviewedTmp++;
-																reviewed.replace(projectName, reviewedTmp);
+																reviewed.replace(program, reviewedTmp);
 															}
 														}
-														console.log(reviewed.get(projectName));
+														console.log(reviewed.get(program));
 														var featureState = feature.get('State');
 														if (featureState != null) {
 															featureStateName = featureState._refObjectName;
 															// console.log(featureStateName);
 															if (featureStateName == "Done") {
-																if (! validated.get(projectName)) {
-																	validated.add(projectName, 1);
+																if (! validated.get(program)) {
+																	validated.add(program, 1);
 																}
 																else {
-																	var validatedTmp = validated.get(projectName);
+																	var validatedTmp = validated.get(program);
 																	validatedTmp++;
-																	validated.replace(projectName, validatedTmp);
+																	validated.replace(program, validatedTmp);
 																}
 															}
-															console.log(validated.get(projectName));
+															console.log(validated.get(program));
 														}
+														
 														// GET USER STORIES - CHECK FOR HLD TAG, ETC.
 													}
 												}
@@ -158,9 +162,40 @@ Ext.define('CustomApp', {
 										}
 										);
 
+										Ext.define('featureData', {
+										    extend: 'Ext.data.Model',
+										    fields: [
+										        {name: 'program', type: 'string'},
+										        {name: 'total', type: 'int'},
+										        {name: 'reviewed', type: 'int'},
+										        {name: 'validated', type: 'int'}
+										    ]
+										});
+										var featureStore = Ext.create('Ext.data.Store', {
+											model: featureData
+										});
+										for (i = 0; i < programs.length; i++) {
+											var programTmp = programs[i];
+											var totalTmp = 0;
+											var reviewedTmp = 0;
+											var validatedTmp = 0;
+											if (total.get(programTmp)) {
+												totalTmp = total.get(programTmp);
+											}
+											if (reviewed.get(programTmp)) {
+												reviewedTmp = reviewed.get(programTmp);
+											}
+											if (validated.get(programTmp)) {
+												validatedTmp = validated.get(programTmp);
+											}
+											
+											featureStore.add({program: programTmp, total: totalTmp, reviewed: reviewedTmp, validated: validatedTmp})
+										}
+
+										console.log(featureStore);
 										console.log(myStore.data.length);
 										
-										this._loadGrid(myStore,total,reviewed,validated);
+										this._loadGrid(featureStore);
 									},
 									scope : this
 								}
@@ -172,24 +207,25 @@ Ext.define('CustomApp', {
 
 	},
 
-	_loadGrid : function(total, reviewed, validated, hld, hldgreenlit) {
+	_loadGrid : function(myStore) {
 
-		// Use Ext.grid.Panel instead of Rally grid
-
-		// Do computation here, create new store from myStore data
-		// https://help.rallydev.com/apps/2.0rc2/doc/#!/api/Ext.grid.Panel
-		console.log("Total: ");
-		console.log(total);
-		console.log("Reviewed: ");
-		console.log(reviewed);
-		console.log("Validated: ");
-		console.log(validated);
-
+		var myGrid = Ext.create('Ext.grid.Panel', {
+		    title: 'PT Dashboard',
+		    store: myStore,
+		    columns: [
+		        { text: 'Program',  dataIndex: 'program' },
+		        { text: 'Total Features', dataIndex: 'total'},
+		        { text: 'Reviewed Features', dataIndex: 'reviewed' },
+		        { text: 'Validated Features', dataIndex: 'validated' }
+		    ]
+		});
+		/**
 		var myGrid = Ext.create('Rally.ui.grid.Grid', {
 			store : myStore,
 			// columnCfgs : [ 'FormattedID', 'Name', 'Release.ReleaseDate' ]
 			columnCfgs : [ 'FormattedID', 'Name', 'Release', 'Project','State', 'c_PATracking' ]
 		});
+		**/
 		console.log("myStore: " + myStore.data.length);
 		console.log(myStore);
 		this.add(myGrid);
